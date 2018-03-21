6cf3ef80d055d173437d5f1460f5e8b528635a0f
emilhThyri <emilThyri@gmail.com>
2018-03-21 11:59:55 +0100

    til 4.4

--- microbit/uart/main.c
+++ microbit/uart/main.c
@@ -3,6 +3,40 @@
 
 int main() {
     printf("mainfil start");
-    uart_init();
-    printf("hooga booga, your file is done");
+
+    // Configure LED Matrix
+	for(int i = 4; i <= 15; i++){
+		GPIO->DIRSET = (1 << i);
+		if (i<13) {
+			GPIO->OUTSET = (1 << i);
+		} else {
+			GPIO->OUTCLR = (1 << i);
+		}
+	}
+
+	int buttonPinA = 17;
+	int buttonPinB = 26;
+	
+	// Configure buttons
+	GPIO->PIN_CNF[buttonPinA] = 0;
+	GPIO->PIN_CNF[buttonPinB] = 0;
+
+
+	uart_init();
+
+
+	while(1){
+
+		int buttonA = !((GPIO->IN)&(1<<buttonPinA));
+		int buttonB = !((GPIO->IN)&(1<<buttonPinB));
+
+		if(buttonA){
+            uart_send('A');
+        }
+        if(buttonB){
+            uart_send('B');
+        }
+	}
+	return 0;
+
 }
